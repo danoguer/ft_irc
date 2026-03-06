218fc0ea10be5ce8b239b2b7e1ece83e92613b48
Andrés Fernández <andfern2@student.42madrid.com>
2026-03-07 00:40:37 +0100

    Allow empty password

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,12 @@
 #include <cstdlib>
 
 int main(int argc, char** argv) {
-    if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " <port> <password>" << std::endl;
+    if (argc < 2 || argc > 3) {
+        std::cerr << "Usage: " << argv[0] << " <port> [password]" << std::endl;
         return 1;
     }
     int port = std::atoi(argv[1]);
-    std::string password = argv[2];
+    std::string password = (argc == 3) ? argv[2] : "";
     try {
         Server server(port, password);
         std::cout << "Starting IRC server on port " << port << std::endl;
